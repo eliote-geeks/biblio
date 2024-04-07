4d0fbae352499251be18f0d249f9d1f064171108
eliote-geeks <pauleliote97@gmail.com>
2024-04-08 00:11:14 +0200

    photo de profil et navbar
    button deconnexion okay avec photos de profil

--- resources/views/components/app.blade.php
+++ resources/views/components/app.blade.php
@@ -52,9 +52,11 @@
     <!-- Navbar -->
     <nav class="navbar navbar-expand-lg navbar-default">
 	<div class="container-fluid px-0">
-		<a class="navbar-brand" href="index.html"
+		<a class="navbar-brand" href="/"
 			><img src="assets/images/brand/logo/logo.svg" alt=""
 		/></a>
+		<!-- Mobile view nav wrap -->
+		
 		<!-- Button -->
 		<button
 			class="navbar-toggler collapsed"
@@ -88,196 +90,20 @@
 						class="dropdown-menu dropdown-menu-arrow"
 						aria-labelledby="navbarBrowse"
 					>
-						<li class="dropdown-submenu dropend">
-							<a
-								class="dropdown-item dropdown-list-group-item dropdown-toggle"
-								href="#"
-							>
-								Web Development
-							</a>
-							<ul class="dropdown-menu">
-								<li>
-									<a
-										class="dropdown-item"
-										href="pages/course-category.html"
-									>
-										Bootstrap</a
-									>
-								</li>
-								<li>
-									<a
-										class="dropdown-item"
-										href="pages/course-category.html"
-									>
-										React
-									</a>
-								</li>
-								<li>
-									<a
-										class="dropdown-item"
-										href="pages/course-category.html"
-									>
-										GraphQl</a
-									>
-								</li>
-								<li>
-									<a
-										class="dropdown-item"
-										href="pages/course-category.html"
-									>
-										Gatsby</a
-									>
-								</li>
-								<li>
-									<a
-										class="dropdown-item"
-										href="pages/course-category.html"
-									>
-										Grunt</a
-									>
-								</li>
-								<li>
-									<a
-										class="dropdown-item"
-										href="pages/course-category.html"
-									>
-										Svelte</a
-									>
-								</li>
-								<li>
-									<a
-										class="dropdown-item"
-										href="pages/course-category.html"
-									>
-										Meteor</a
-									>
-								</li>
-								<li>
-									<a
-										class="dropdown-item"
-										href="pages/course-category.html"
-									>
-										HTML5</a
-									>
-								</li>
-								<li>
-									<a
-										class="dropdown-item"
-										href="pages/course-category.html"
-									>
-										Angular</a
-									>
-								</li>
-							</ul>
-						</li>
-						<li class="dropdown-submenu dropend">
-							<a
-								class="dropdown-item dropdown-list-group-item dropdown-toggle"
-								href="#"
-							>
-								Design
-							</a>
-							<ul class="dropdown-menu">
-								<li>
-									<a
-										class="dropdown-item"
-										href="pages/course-category.html"
-									>
-										Graphic Design</a
-									>
-								</li>
-								<li>
-									<a
-										class="dropdown-item"
-										href="pages/course-category.html"
-									>
-										Illustrator
-									</a>
-								</li>
-								<li>
-									<a
-										class="dropdown-item"
-										href="pages/course-category.html"
-									>
-										UX / UI Design</a
-									>
-								</li>
-								<li>
-									<a
-										class="dropdown-item"
-										href="pages/course-category.html"
-									>
-										Figma Design</a
-									>
-								</li>
-								<li>
-									<a
-										class="dropdown-item"
-										href="pages/course-category.html"
-									>
-										Adobe XD</a
-									>
-								</li>
-								<li>
-									<a
-										class="dropdown-item"
-										href="pages/course-category.html"
-									>
-										Sketch</a
-									>
-								</li>
-								<li>
-									<a
-										class="dropdown-item"
-										href="pages/course-category.html"
-									>
-										Icon Design</a
-									>
-								</li>
-								<li>
-									<a
-										class="dropdown-item"
-										href="pages/course-category.html"
-									>
-										Photoshop</a
-									>
-								</li>
-							</ul>
-						</li>
+					@foreach (\App\Models\Category::all() as $cat)
+										
 						<li>
 							<a
 								href="pages/course-category.html"
 								class="dropdown-item"
 							>
-								Mobile App
+								{{ $cat->name }}
 							</a>
 						</li>
-						<li>
-							<a
-								href="pages/course-category.html"
-								class="dropdown-item"
-							>
-								IT Software
-							</a>
-						</li>
-						<li>
-							<a
-								href="pages/course-category.html"
-								class="dropdown-item"
-							>
-								Marketing
-							</a>
-						</li>
-						<li>
-							<a
-								href="pages/course-category.html"
-								class="dropdown-item"
-							>
-								Music
-							</a>
-						</li>
-						
-				
+						@endforeach
+					</ul>
+				</li>
+		
 			</ul>
 			<form class="mt-3 mt-lg-0 ms-lg-3 d-flex align-items-center">
 				<span class="position-absolute ps-3 search-icon">
@@ -289,9 +115,8 @@
 					placeholder="Search Courses"
 				/>
 			</form>
-
 			<ul class="navbar-nav navbar-right-wrap ms-auto d-none d-lg-block">
-				
+			
 
 				<li class="dropdown ms-2 d-inline-block">
 					<a
@@ -301,13 +126,30 @@
 						data-bs-display="static"
 						aria-expanded="false"
 					>
-					
+						<div class="avatar avatar-md avatar-indicators avatar-online">
+							<img
+								alt="avatar"
+								@auth
+								src="{{ Auth::user()->profile_photo_url }}"
+								@endauth
+
+								@guest 
+								src="assets/images/brand/logo/logo.svg"
+								@endguest
+								class="rounded-circle"
+							/>
+						</div>
 					</a>
+					@auth
 					<div class="dropdown-menu dropdown-menu-end">
 						<div class="dropdown-item">
 							<div class="d-flex">
 								<div class="avatar avatar-md avatar-indicators avatar-online">
-									
+									<img
+										alt="avatar"
+										src="{{ Auth::user()->profile_photo_url }}"
+										class="rounded-circle"
+									/>
 								</div>
 								<div class="ms-3 lh-1">
 									<h5 class="mb-1">{{ Auth::user()->name }}</h5>
@@ -316,16 +158,43 @@
 							</div>
 						</div>
 						<div class="dropdown-divider"></div>
-						
+						<ul class="list-unstyled">
+							<li>
+								<a
+									class="dropdown-item"
+									href="pages/profile-edit.html"
+								>
+									<i class="fe fe-user me-2"></i>Profile
+								</a>
+							</li>
+
+						</ul>
 						<div class="dropdown-divider"></div>
 						<ul class="list-unstyled">
 							<li>
-								<a class="dropdown-item" href="index.html">
-									<i class="fe fe-power me-2"></i>Sign Out
+								<form class="dropdown-item" method="POST" action="{{ route('logout') }}">
+									@csrf
+									<div class="nav-item">
+										<a href="{{ route('logout') }}" onclick="event.preventDefault(); this.closest('form').submit();" role="button">Sign Out</a>
+									</div>
+								</form>
+							</li>
+						</ul>
+					</div>
+					@endauth
+
+					@guest 
+					<div class="dropdown-menu dropdown-menu-end">
+						
+						<ul class="list-unstyled">
+							<li>
+								<a class="dropdown-item" href="{{ route('login') }}">
+									<i class="fe fe-power me-2"></i>Login
 								</a>
 							</li>
 						</ul>
 					</div>
+					@endguest
 				</li>
 			</ul>
 		</div>
@@ -339,15 +208,15 @@
         <div class="row align-items-center g-0 border-top py-2">
             <!-- Desc -->
             <div class="col-md-6 col-12 text-center text-md-start">
-                <span>© 2022 Geeks. All Rights Reserved.</span>
+                <span>© {{ date('Y') }} Geeks. All Rights Reserved.</span>
             </div>
               <!-- Links -->
             <div class="col-12 col-md-6">
                 <nav class="nav nav-footer justify-content-center justify-content-md-end">
-                    <a class="nav-link active ps-0" href="#">Privacy</a>
-                    <a class="nav-link" href="#">Terms </a>
-                    <a class="nav-link" href="#">Feedback</a>
-                    <a class="nav-link" href="#">Support</a>
+                    <a class="nav-link active ps-0" href="javascript:;">Privacy</a>
+                    <a class="nav-link" href="javascript:;">Terms </a>
+                    <a class="nav-link" href="javascript:;">Feedback</a>
+                    <a class="nav-link" href="javascript:;">Support</a>
                 </nav>
             </div>
         </div>
